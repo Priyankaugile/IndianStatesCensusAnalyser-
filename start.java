2b2f19f65648f29550d1bf5f97849c5284e5ab81
priyankaugile <ugilepriyanka26@gmail.com>
2021-09-29 10:24:17 +0530

    Added: Welcome to Indian states census program.

---
+++ start.java
@@ -0,0 +1,9 @@
+package com.indianstatescensus;
+
+public class start {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States\n" +
+                "Census Analyser\n" +
+                "Problem.");
+    }
+}
